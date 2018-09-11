d84ffa82818949d2b0ea50cd0472ed2062bfcd5d
Black_Zerg <blackzerg@gmail.com>
2018-09-12 00:14:27 +0400

    Application read and parse list of currencies

--- app/src/main/java/studio/bz_soft/currencyrates/MainActivity.java
+++ app/src/main/java/studio/bz_soft/currencyrates/MainActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    public static final String API_URL = "http://www.nbrb.by/API/ExRates/";
+    public static final String CURRENCIES = "Currencies";
+    public static final String RATES = "Rates";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/studio/bz_soft/currencyrates/SplashScreenActivity.java
+++ app/src/main/java/studio/bz_soft/currencyrates/SplashScreenActivity.java
@@ -4,13 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import studio.bz_soft.currencyrates.net.RequestData;
+import studio.bz_soft.currencyrates.net.WebServices;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private final static String TAG = SplashScreenActivity.class.getSimpleName();
+
+    private static final String METHOD_GET = "GET";
+    private static final String DATA = "";
+    private static final String COMMAND = "";
+
+    private static final String ERROR_MESSAGE_INTERNET = "No INTERNET connection!..";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(this, MainActivity.class);
+        String URL = String.format("%s%s", MainActivity.API_URL, MainActivity.CURRENCIES);
+        try {
+            Object obj = WebServices.getInstance().
+                    requestInfo(new RequestData(URL, DATA, METHOD_GET, COMMAND),
+                            WebServices.SELECTOR_CURRENCIES);
+            Log.d(TAG, "Object: " + obj);
+        } catch (NullPointerException ex) {
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, ERROR_MESSAGE_INTERNET);
+            }
+        }
+
         startActivity(intent);
         finish();
     }
