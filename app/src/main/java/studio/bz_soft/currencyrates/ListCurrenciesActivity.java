26555ea333fcbae69bb0418f062539b8af1ad2c0
Black_Zerg <blackzerg@gmail.com>
2018-09-14 01:22:39 +0400

    Added strings for Russian language Added layout for ListView row Changed menu style

--- app/src/main/java/studio/bz_soft/currencyrates/ListCurrenciesActivity.java
+++ app/src/main/java/studio/bz_soft/currencyrates/ListCurrenciesActivity.java
@@ -2,12 +2,18 @@ package studio.bz_soft.currencyrates;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ListCurrenciesActivity extends AppCompatActivity {
+
+
+    private ListView listViewCurrencies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_currencies);
+
+        listViewCurrencies = findViewById(R.id.listViewCurrencies);
     }
 }
