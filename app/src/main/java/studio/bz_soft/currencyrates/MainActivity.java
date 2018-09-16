60a21d178776a705efaa53f7f2b7ed5e10bbf5d9
Black_Zerg <blackzerg@gmail.com>
2018-09-16 14:34:08 +0400

    Removed garbage from code

--- app/src/main/java/studio/bz_soft/currencyrates/MainActivity.java
+++ app/src/main/java/studio/bz_soft/currencyrates/MainActivity.java
@@ -7,28 +7,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.List;
-
-import studio.bz_soft.currencyrates.model.Currency;
-
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = MainActivity.class.getSimpleName();
 
-    public static final String API_URL = "http://www.nbrb.by/API/ExRates/";
-    public static final String CURRENCIES = "Currencies";
-    public static final String RATES = "RatesInterface";
-
     private Intent intent;
-    private List<Currency> listCurrency;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_list_currencies:
                 Intent intent = new Intent(this, ListCurrenciesActivity.class);
-//                ArrayList<? extends Currency> currencyList = this.intent.getParcelableArrayListExtra(SplashScreenActivity.INTENT_CURRENCIES_KEY);
-//                intent.putParcelableArrayListExtra(SplashScreenActivity.INTENT_CURRENCIES_KEY, currencyList);
                 startActivity(intent);
                 break;
         }
@@ -49,6 +38,5 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         intent = getIntent();
-//        listCurrency = intent.getParcelableArrayListExtra(SplashScreenActivity.INTENT_CURRENCIES_KEY);
     }
 }
