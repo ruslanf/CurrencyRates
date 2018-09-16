0a5a64b8e9803d07a031ecd138a0266a1b0b9f6a
Black_Zerg <blackzerg@gmail.com>
2018-09-16 20:34:35 +0400

    New classes for store Image ListCurrenciesActivity - load list of currencies with images

--- app/src/main/java/studio/bz_soft/currencyrates/ListCurrenciesActivity.java
+++ app/src/main/java/studio/bz_soft/currencyrates/ListCurrenciesActivity.java
@@ -1,5 +1,7 @@
 package studio.bz_soft.currencyrates;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,7 +9,22 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
-import rx.schedulers.Schedulers;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import studio.bz_soft.currencyrates.controller.CurrencyAdapter;
+import studio.bz_soft.currencyrates.model.Currency;
+import studio.bz_soft.currencyrates.model.Image;
+import studio.bz_soft.currencyrates.model.ImageList;
 import studio.bz_soft.currencyrates.network.NetworkModule;
 import studio.bz_soft.currencyrates.network.CurrencyInterface;
 import studio.bz_soft.currencyrates.view.ViewInterface;
@@ -16,8 +33,19 @@ public class ListCurrenciesActivity extends AppCompatActivity implements ViewInt
 
     private static final String TAG = ListCurrenciesActivity.class.getSimpleName();
 
+    private static final String ASSETS_IMAGE_FOLDER = "icons-48";
+
     private ListView listViewCurrencies;
     private ProgressBar progressBar;
+    private CompositeDisposable compositeDisposable;
+
+    private ImageList imageList;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.dispose();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +54,37 @@ public class ListCurrenciesActivity extends AppCompatActivity implements ViewInt
 
         listViewCurrencies = findViewById(R.id.listViewCurrencies);
         progressBar = findViewById(R.id.progressBar);
+        showWait();
+
+        Observable<List<Image>> observableImageList = Observable.fromCallable(new Callable<List<Image>>() {
+            @Override
+            public List<Image> call() throws Exception {
+                List<Image> list = new ArrayList<>();
+
+                String[] imageFolder = getAssets().list(ASSETS_IMAGE_FOLDER);
+                if (imageFolder.length > 0) {
+                    for (int i = 0; i < imageFolder.length; i++) {
+                        String filename = String.format("%s/%s", ASSETS_IMAGE_FOLDER, imageFolder[i]);
+                        String code = imageFolder[i].substring(0, 2);
+                        InputStream inputStream = getAssets().open(filename);
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        list.add(new Image(code, bitmap));
+                    }
+                } else {
+                    if (BuildConfig.DEBUG) {
+                        Log.d(TAG, "No images in asset folder...");
+                    }
+                }
+                return list;
+            }
+        });
+        Disposable disposable =
+        observableImageList
+                .subscribeOn(Schedulers.io())
+                .subscribe(list -> imageList = new ImageList(list));
+
+        compositeDisposable = new CompositeDisposable();
+        compositeDisposable.add(disposable);
     }
 
     @Override
@@ -35,40 +94,28 @@ public class ListCurrenciesActivity extends AppCompatActivity implements ViewInt
             Log.d(TAG, "onStart()...");
         }
 
-//        RxJavaCallAdapterFactory rxJavaCallAdapterFactory = RxJavaCallAdapterFactory.createWithScheduler(rx.schedulers.Schedulers.io());
-//        Retrofit retrofit = new Retrofit.Builder()
-//                .baseUrl("http://www.nbrb.by/API/ExRates/")
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .addCallAdapterFactory(rxJavaCallAdapterFactory)
-//                .build();
-//
-//        CurrencyInterface currencyInterface = retrofit.create(CurrencyInterface.class);
-
-//        Observable<List<Currency>> observable = currencyInterface.listCurrencies();
-//        observable.subscribeOn(Schedulers.io()).subscribe();
-
         CurrencyInterface retrofitApi = NetworkModule.getRetrofit().create(CurrencyInterface.class);
         retrofitApi.listCurrencies()
-                .subscribeOn(Schedulers.io())
-                .subscribe();
+                .enqueue(new Callback<List<Currency>>() {
+                    @Override
+                    public void onResponse(Call<List<Currency>> call, Response<List<Currency>> response) {
+                        if (response.body() != null) {
+                            CurrencyAdapter currencyAdapter = new CurrencyAdapter(ListCurrenciesActivity.this,
+                                    getApplicationContext(),
+                                    R.layout.row_currencies_layout,
+                                    R.id.textViewAbbreviation,
+                                    R.id.textViewCurrencyName,
+                                    response.body(), imageList);
+                            listViewCurrencies.setAdapter(currencyAdapter);
+                        }
+                        removeWait();
+                    }
 
-//        currencyInterface.listCurrencies().enqueue(new Callback<List<Currency>>() {
-//            @Override
-//            public void onResponse(Call<List<Currency>> call, Response<List<Currency>> response) {
-//                CurrencyAdapter currencyAdapter = new CurrencyAdapter(ListCurrenciesActivity.this,
-//                        getApplicationContext(),
-//                        R.layout.row_currencies_layout,
-//                        R.id.textViewAbbreviation,
-//                        R.id.textViewCurrencyName,
-//                        response.body());
-//                listViewCurrencies.setAdapter(currencyAdapter);
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Currency>> call, Throwable t) {
-//
-//            }
-//        });
+                    @Override
+                    public void onFailure(Call<List<Currency>> call, Throwable t) {
+
+                    }
+                });
     }
 
     @Override
