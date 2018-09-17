3b250709f2b0849a9678bc600540faacae3ee125
Black_Zerg <blackzerg@gmail.com>
2018-09-18 02:43:53 +0400

    Refactoring code for optimize classes

--- app/src/main/java/studio/bz_soft/currencyrates/model/Currency.java
+++ app/src/main/java/studio/bz_soft/currencyrates/model/Currency.java
@@ -1,30 +1,28 @@
 package studio.bz_soft.currencyrates.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.support.annotation.Nullable;
 
 import com.google.gson.annotations.SerializedName;
 
-public class Currency implements Parcelable {
+public final class Currency {
 
     @SerializedName("Cur_ID")
-    private int curID;
+    private final int curID;
     @Nullable
     @SerializedName("Cur_ParentID")
-    private int curParentID;
+    private final int curParentID;
     @SerializedName("Cur_Code")
-    private String curCode;
+    private final String curCode;
     @SerializedName("Cur_Abbreviation")
-    private String curAbbreviation;
+    private final String curAbbreviation;
     @SerializedName("Cur_Name")
-    private String curName;
+    private final String curName;
     @SerializedName("Cur_Name_Bel")
     private String curNameBel;
     @SerializedName("Cur_Name_Eng")
     private String curNameEng;
     @SerializedName("Cur_QuotName")
-    private String curQuotName;
+    private final String curQuotName;
     @SerializedName("Cur_QuotName_Bel")
     private String curQuotNameBel;
     @SerializedName("Cur_QuotName_Eng")
@@ -46,11 +44,8 @@ public class Currency implements Parcelable {
     @SerializedName("Cur_DateEnd")
     private String curDateEnd;
 
-    public Currency() {
-
-    }
-
-    public Currency(int curID, int curParentID, String curCode, String curAbbreviation, String curName, String curQuotName) {
+    public Currency(final int curID, final int curParentID, final String curCode,
+                    final String curAbbreviation, final String curName, final String curQuotName) {
         this.curID = curID;
         this.curParentID = curParentID;
         this.curCode = curCode;
@@ -59,197 +54,74 @@ public class Currency implements Parcelable {
         this.curQuotName = curQuotName;
     }
 
-    protected Currency(Parcel in) {
-        curID = in.readInt();
-        curParentID = in.readInt();
-        curCode = in.readString();
-        curAbbreviation = in.readString();
-        curName = in.readString();
-        curNameBel = in.readString();
-        curNameEng = in.readString();
-        curQuotName = in.readString();
-        curQuotNameBel = in.readString();
-        curQuotNameEng = in.readString();
-        curNameMulti = in.readString();
-        curNameBelMulti = in.readString();
-        curNameEngMulti = in.readString();
-        curScale = in.readInt();
-        curPeriodicity = in.readInt();
-        curDateStart = in.readString();
-        curDateEnd = in.readString();
-    }
-
-    public static final Creator<Currency> CREATOR = new Creator<Currency>() {
-        @Override
-        public Currency createFromParcel(Parcel in) {
-            return new Currency(in);
-        }
-
-        @Override
-        public Currency[] newArray(int size) {
-            return new Currency[size];
-        }
-    };
-
     public int getCurID() {
         return curID;
     }
 
-    public void setCurID(int curID) {
-        this.curID = curID;
-    }
-
+    @Nullable
     public int getCurParentID() {
         return curParentID;
-    }
-
-    public void setCurParentID(int curParentID) {
-        this.curParentID = curParentID;
     }
 
     public String getCurCode() {
         return curCode;
     }
 
-    public void setCurCode(String curCode) {
-        this.curCode = curCode;
-    }
-
     public String getCurAbbreviation() {
         return curAbbreviation;
-    }
-
-    public void setCurAbbreviation(String curAbbreviation) {
-        this.curAbbreviation = curAbbreviation;
     }
 
     public String getCurName() {
         return curName;
     }
 
-    public void setCurName(String curName) {
-        this.curName = curName;
-    }
-
     public String getCurNameBel() {
         return curNameBel;
-    }
-
-    public void setCurNameBel(String curNameBel) {
-        this.curNameBel = curNameBel;
     }
 
     public String getCurNameEng() {
         return curNameEng;
     }
 
-    public void setCurNameEng(String curNameEng) {
-        this.curNameEng = curNameEng;
-    }
-
     public String getCurQuotName() {
         return curQuotName;
-    }
-
-    public void setCurQuotName(String curQuotName) {
-        this.curQuotName = curQuotName;
     }
 
     public String getCurQuotNameBel() {
         return curQuotNameBel;
     }
 
-    public void setCurQuotNameBel(String curQuotNameBel) {
-        this.curQuotNameBel = curQuotNameBel;
-    }
-
     public String getCurQuotNameEng() {
         return curQuotNameEng;
-    }
-
-    public void setCurQuotNameEng(String curQuotNameEng) {
-        this.curQuotNameEng = curQuotNameEng;
     }
 
     public String getCurNameMulti() {
         return curNameMulti;
     }
 
-    public void setCurNameMulti(String curNameMulti) {
-        this.curNameMulti = curNameMulti;
-    }
-
+    @Nullable
     public String getCurNameBelMulti() {
         return curNameBelMulti;
     }
 
-    public void setCurNameBelMulti(String curNameBelMulti) {
-        this.curNameBelMulti = curNameBelMulti;
-    }
-
+    @Nullable
     public String getCurNameEngMulti() {
         return curNameEngMulti;
-    }
-
-    public void setCurNameEngMulti(String curNameEngMulti) {
-        this.curNameEngMulti = curNameEngMulti;
     }
 
     public int getCurScale() {
         return curScale;
     }
 
-    public void setCurScale(int curScale) {
-        this.curScale = curScale;
-    }
-
     public int getCurPeriodicity() {
         return curPeriodicity;
-    }
-
-    public void setCurPeriodicity(int curPeriodicity) {
-        this.curPeriodicity = curPeriodicity;
     }
 
     public String getCurDateStart() {
         return curDateStart;
     }
 
-    public void setCurDateStart(String curDateStart) {
-        this.curDateStart = curDateStart;
-    }
-
     public String getCurDateEnd() {
         return curDateEnd;
-    }
-
-    public void setCurDateEnd(String curDateEnd) {
-        this.curDateEnd = curDateEnd;
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(curID);
-        dest.writeInt(curParentID);
-        dest.writeString(curCode);
-        dest.writeString(curAbbreviation);
-        dest.writeString(curName);
-        dest.writeString(curNameBel);
-        dest.writeString(curNameEng);
-        dest.writeString(curQuotName);
-        dest.writeString(curQuotNameBel);
-        dest.writeString(curQuotNameEng);
-        dest.writeString(curNameMulti);
-        dest.writeString(curNameBelMulti);
-        dest.writeString(curNameEngMulti);
-        dest.writeInt(curScale);
-        dest.writeInt(curPeriodicity);
-        dest.writeString(curDateStart);
-        dest.writeString(curDateEnd);
     }
 }
--- app/src/main/java/studio/bz_soft/currencyrates/model/Rates.java
+++ app/src/main/java/studio/bz_soft/currencyrates/model/Rates.java
@@ -1,28 +1,24 @@
 package studio.bz_soft.currencyrates.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-import android.support.annotation.Nullable;
-
 import com.google.gson.annotations.SerializedName;
 
-public class Rates implements Parcelable {
+public final class Rates {
 
     @SerializedName("Cur_ID")
-    private int curID;
+    private final int curID;
     @SerializedName("Date")
-    private String date;
+    private final String date;
     @SerializedName("Cur_Abbreviation")
-    private String curAbbreviation;
+    private final String curAbbreviation;
     @SerializedName("Cur_Scale")
-    private int curScale;
+    private final int curScale;
     @SerializedName("Cur_Name")
-    private String curName;
-    @Nullable
+    private final String curName;
     @SerializedName("Cur_OfficialRate")
-    private double curOfficialRate;
+    private final double curOfficialRate;
 
-    public Rates(int curID, String date, String curAbbreviation, int curScale, String curName, double curOfficialRate) {
+    public Rates(final int curID, final String date, final String curAbbreviation,
+                 final int curScale, final String curName, final double curOfficialRate) {
         this.curID = curID;
         this.date = date;
         this.curAbbreviation = curAbbreviation;
@@ -31,87 +27,27 @@ public class Rates implements Parcelable {
         this.curOfficialRate = curOfficialRate;
     }
 
-    protected Rates(Parcel in) {
-        curID = in.readInt();
-        date = in.readString();
-        curAbbreviation = in.readString();
-        curScale = in.readInt();
-        curName = in.readString();
-        curOfficialRate = in.readFloat();
-    }
-
-    public static final Creator<Rates> CREATOR = new Creator<Rates>() {
-        @Override
-        public Rates createFromParcel(Parcel in) {
-            return new Rates(in);
-        }
-
-        @Override
-        public Rates[] newArray(int size) {
-            return new Rates[size];
-        }
-    };
-
     public int getCurID() {
         return curID;
-    }
-
-    public void setCurID(int curID) {
-        this.curID = curID;
     }
 
     public String getDate() {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-
     public String getCurAbbreviation() {
         return curAbbreviation;
-    }
-
-    public void setCurAbbreviation(String curAbbreviation) {
-        this.curAbbreviation = curAbbreviation;
     }
 
     public int getCurScale() {
         return curScale;
     }
 
-    public void setCurScale(int curScale) {
-        this.curScale = curScale;
-    }
-
     public String getCurName() {
         return curName;
     }
 
-    public void setCurName(String curName) {
-        this.curName = curName;
-    }
-
     public double getCurOfficialRate() {
         return curOfficialRate;
-    }
-
-    public void setCurOfficialRate(float curOfficialRate) {
-        this.curOfficialRate = curOfficialRate;
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(curID);
-        dest.writeString(date);
-        dest.writeString(curAbbreviation);
-        dest.writeInt(curScale);
-        dest.writeString(curName);
-        dest.writeDouble(curOfficialRate);
     }
 }
