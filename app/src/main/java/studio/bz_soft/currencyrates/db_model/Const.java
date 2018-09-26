32abb43433eb976df7139a9a5e027597379352ed
Black_Zerg <blackzerg@gmail.com>
2018-09-26 18:12:13 +0400

    class for Constants

---
+++ app/src/main/java/studio/bz_soft/currencyrates/db_model/Const.java
@@ -0,0 +1,27 @@
+package studio.bz_soft.currencyrates.db_model;
+
+final class Const {
+
+    public static final String DB_NAME = "currency_database";
+
+    public final static String CREATE_TABLE_CURRENCIES = "CREATE TABLE IF NOT EXISTS `Currencies` " +
+            "( `_id` INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "`curID` INTEGER NOT NULL, " +
+            "`curParentID` INTEGER NOT NULL, " +
+            "`curCode` TEXT NOT NULL, " +
+            "`curAbbreviation` TEXT NOT NULL, " +
+            "`curName` TEXT NOT NULL, " +
+            "`curQuotName` TEXT NOT NULL " +
+            ");";
+
+    public final static String CREATE_TABLE_RATES = "CREATE TABLE IF NOT EXISTS `Rates` " +
+            "( `_id` INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "`curID` INTEGER NOT NULL, " +
+            "`date` TEXT NOT NULL, " +
+            "`curAbbreviation` TEXT NOT NULL, " +
+            "`curScale` INTEGER NOT NULL, " +
+            "`curName` TEXT NOT NULL, " +
+            "`curOfficialRate` NUMERIC NOT NULL " +
+            ");";
+
+}
